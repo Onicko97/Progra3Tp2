c7f2d16747cabd63740de80243a653f4606894e7
Tomas <102042076+Pungsis@users.noreply.github.com>
2026-04-22 14:02:37 -0300

    agregada implementacion de AGM (MST en ingles)

--- Progra3Tp2/src/main/logic/MST.java
+++ Progra3Tp2/src/main/logic/MST.java
@@ -2,15 +2,54 @@ package logic;
 
 import models.grafo.Grafo;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import models.grafo.Arista;
 public class MST<T> {
-	
-	public void kruskal(Grafo<T> g, T inicio) {
-		int[] verticesPadre = makeSet(g.getVertices().size());
-		Grafo<T> arbolGeneradorMinimo = new Grafo<T>(null);
+	private Grafo<T> g;
+	private int[] A;
+	public MST(Grafo<T> g) {
+		
+		this.g = g;
+		this.A = makeSet(g.getVertices().size());
+	}
+	public Grafo<T> kruskal(T inicio) {
+		 
+		List<Arista> aristasOrdenadas = ordenarAristas(g.getAristas());
+		Grafo<T> arbolGeneradorMinimo = new Grafo<T>(g.getVertices());
+		int pesoTotal = 0;
+		for(Arista ar: aristasOrdenadas ) {
+			int origen = ar.getOrigen();
+			int destino = ar.getDestino();
+			double peso = ar.getPeso();
+			if(!mismaComponenteConexa( origen, destino)) {
+				pesoTotal += peso;
+				union(origen, destino);
+				arbolGeneradorMinimo.agregarArista(origen, destino, peso);
+			}
+		}
+		return arbolGeneradorMinimo;
 	}
 	
-	public void union() {}
-	public void find() {}
+	// se verifica si estan en la misma componente conexa
+	private boolean mismaComponenteConexa(int i,int j) {
+		return find(A, i) == find (A, j);
+	}
+	public void union(int i, int j) {
+		int raiz1 = find(A, i);
+		int raiz2 = find(A, j);
+		//actaulizamos la raiz de uno para que sea la raiz del otro
+		A[raiz1] = raiz2;
+	}
+	//encuentra la raiz del vertice
+	public int find(int[] A, int i) {
+		if (i == A[i] ) {
+			return i;
+		}
+		return find ( A, A[i]);
+	}
 	// inicializamos cada componentes - 
 	private int[] makeSet(int cantVertices) {
 		int[] verticesPadre = new int[cantVertices];
@@ -20,6 +59,10 @@ public class MST<T> {
 		return verticesPadre;
 	}
 	// se crea un diccionario con <Arista, double> con peso de menor a mayor
-	//public HashMap<Arista, double> ordenarAristas() {}
+	private List<Arista> ordenarAristas(List<Arista> aristas) {
+		return aristas.stream()	
+				.sorted(Comparator.comparingDouble(Arista::getPeso))
+				.collect(Collectors.toList());
+		}
 	
 }
--- Progra3Tp2/src/main/models/grafo/Arista.java
+++ Progra3Tp2/src/main/models/grafo/Arista.java
@@ -13,4 +13,30 @@ public class Arista {
 			this.peso = peso;;
 		}
 
+		public int getOrigen() {
+			return origen;
+		}
+
+		public void setOrigen(int origen) {
+			this.origen = origen;
+		}
+
+		public int getDestino() {
+			return destino;
+		}
+
+		public void setDestino(int destino) {
+			this.destino = destino;
+		}
+
+		public double getPeso() {
+			return peso;
+		}
+
+		public void setPeso(double peso) {
+			this.peso = peso;
+		}
+		
+		
+
 }
