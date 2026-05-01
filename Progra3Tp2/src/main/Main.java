8fd33ed2d83d22a8258f641c67e40063ac3b80f7
Nick97 <nicobilchez@gmail.com>
2026-05-01 11:11:41 -0300

    arregle las aristas y agregue un JPanel para poner botones

--- Progra3Tp2/src/main/Main.java
+++ Progra3Tp2/src/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		VentanaPrincipal ventana = new VentanaPrincipal();
+		ventana.probarVerticesAristas();//para probar, despues hay que borrarlo
 		ventana.mostrar();//provisorio, va en presenter
 		System.out.println("hello world");
 	}
--- Progra3Tp2/src/main/models/domain/Localidad.java
+++ Progra3Tp2/src/main/models/domain/Localidad.java
@@ -1,12 +1,16 @@
 package models.domain;
 
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+
 public class Localidad {
-	private double latitud;
-	private double longitud;
+	
+	private Coordinate coordenadas;
 	private String nombre;
 	private String provincia;
 	
 	public Localidad(String nombre, String provincia, double latitud, double longitud) {
-		
+		this.nombre = nombre;
+		this.provincia = provincia;
+		this.coordenadas = new Coordinate(latitud, longitud);
 	}
 }
